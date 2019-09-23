806fc478c39213b5c09816b312c42fdb5fc69bab
marktheawesome <markgyomory@outlook.com>
2019-09-23 19:17:38 -0400

    working on rover now

--- mars-rover/Rover.java
+++ mars-rover/Rover.java
@@ -9,9 +9,11 @@ public class Rover
 {
     // instance variables
     private String name;
-    private int x;
-    private int y;
-    private int dir;
+    private double x;
+    private double y;
+    private double dir;
+    private double x0;
+    private double y0;
 
     /**
      * Constructor for objects of class Rover
@@ -23,68 +25,123 @@ public class Rover
         this.dir = 0;
     }
 
-    // methods
-    public void move(int magnitude) {
-        // if (dir == 0) {
-        //     y = y + magnitude;
-        // }
-        // else if (dir == 1) {
-        //     x = x  + magnitude;
-        // }
-        // else if (dir == 2) {
-        //     y = y  - magnitude;
-        // }
-        // else if (dir == 3) {
-        //     x = x  - magnitude;
-        // }
-            y =0;
-        System.out.println(name + " moved forward " + magnitude + "  American Freedom units.");
+    public Rover(String name, int x, int y){
+        this.name = name;
+        this.x0 = x;
+        this.y0 = y;
+        this.x = x;
+        this.y = y;
+        this.dir = 0;
     }
 
-    // public void rotateRight() {
-    //     dir = dir + 1;
+    // methods
+    private void position(){
+        System.out.println(name + "," + " x = " + this.x + "," + " Y = " + this.y + "," + " Dir = " + this.dir);
+    }
 
-    //     if(dir == 4){
-    //         dir = 0;
-    //     }
+    public void move(int magnitude) {
+        if (dir % 2 == 1){
+            y = y + magnitude;
+            x = x + magnitude;
+        }
+        if (dir == 0) {
+            y = y + magnitude;
+        }
+        else if (dir == 2) {
+            x = x  + magnitude;
+        }
+        else if (dir == 4) {
+            y = y  - magnitude;
+        }
+        else if (dir == 6) {
+            x = x  - magnitude;
+        }
+        this. position();
 
-    //     System.out.println(name + " rotated to the right, and is facing " + getDirectionName());
-    // }
-
-    // public void rotateLeft() {
-    //     dir = dir - 1;
-
-    //     if(dir == -1){
-    //         dir = 3;
-    //     }
-    //     System.out.println(name + " rotated to the left, and is facing " + getDirectionName());
-    // }
-
+    }
 
     public void rotate(double deg){
-        this.dir += deg;
+        dir = dir + deg;
 
-        if (dir < 0){
-            dir = 360 + dir;
+        while(dir < 0)
+        {
+            dir = dir + 8;
         }
-        System.out.println(this.name + " has the bearing " + dir + " deg");
+        while(dir > 7)
+        {
+            dir = dir - 8;
+        }
+
+        System.out.println(name + " is facing " + getDirectionName());
+    }
+
+    public void rotateTo(double newDir){
+        double diff = dir - newDir;
+        rotate(diff);
     }
 
     private String getDirectionName(){
         if (this.dir == 0){
             return "North";
         }
-        else if (this.dir == 1){
-            return "East";
+        else if(this.dir == 1){
+            return "Northeast";
         }
         else if (this.dir == 2){
-            return "South";
+            return "East";
         }
         else if (this.dir == 3){
+            return "Southeast";
+        }
+        else if (this.dir == 4){
+            return "South";
+        }
+        else if (this.dir == 5){
+            return "Southwest";
+        }
+        else if (this.dir == 6){
             return "West";
+        }
+        else if (this.dir == 7){
+            return "Northwest";
         }
 
         return "This shuld never be returned.";
+    }
+
+    public void teleport(int newX, int newY){
+        this.x = newX;
+        this.y = newY;
+        this.position();
+    }
+
+    public void moveTo(int newX, int newY){
+        double dx = newX - this.x;
+        double dy = newY - this.y;
+
+        if(dy == 0){
+
+            this.x = this.x + dx;
+        }
+
+        else if (dx == 0){
+            this.y = this.y + dy;
+        }
+
+        double slope = dy/dx;
+
+        if (slope == 1 || slope ==-1){
+            this.x = this.x + dx;
+            this.y = this.y + dy;
+        }
+
+
+        position();
+    }
+
+    public void goHome(){
+        this.x = this.x0;
+        this.y = this.x0;
     }
 }
 
--- mars-rover/Runner.java
+++ mars-rover/Runner.java
@@ -10,14 +10,24 @@ public class Runner
     public static void main(String[] args) {
         Rover r1 = new Rover("Aryan");
         Rover r2 = new Rover("Craig");
+        Rover r3 = new Rover("Mark");
+        Rover r4 = new Rover("Cole");
+        Rover r5 = new Rover("Sky");
 
-        r1.move(10);
-        //r2.rotateRight();
-        r2.rotate(91);
-        r2.move(2);
-        //r1.rotateRight();
-        r1.rotate(-91);
-        r1.move(4);
-        //r2.rotateLeft();
+        // r1.move(10);
+        // r2.rotate(91);
+        // r2.move(2);
+        // r1.rotate(-91);
+        // r1.move(4);
+        // r1.rotate(280);
+        // r1.rotate(-1);
+        // r1.move(2);
+
+        // r1.teleport(2,2);
+        r1.moveTo(10,0);
+        r2.moveTo(0,10);
+        r3.moveTo(10, 10);
+        r4.moveTo(-10,-10);
+        r5.moveTo(-10,10);
     }
 }
